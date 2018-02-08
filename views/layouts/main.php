5d5fa13a94bdecc63873411d968b0e6f7c9e2127
Your Name <you@example.com>
2018-02-08 12:00:50 +0200

    добавил второе меню товаров с цитруса в хедер

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,72 +27,44 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div class="container">
-    <table>
-        <tr>
-            <div class="row topmenu">
-                <div class="col-xs-4" id="citrus_top_menu">
-                    <?php
-                    Menu::begin([
-                        'options' => [
-                            'class' => 'navbar-inverse navbar-fixed-top',
-                        ],
-                    ]);
-                    echo Menu::widget([
-                        'items' => [
-                            ['label' => 'Главная', 'url' => ['site/index']],
-                            ['label' => 'О компании', 'url' => ['site/about']],
-                            ['label' => 'Услуги', 'url' => ['site/services']],
-                            ['label' => 'Контакты', 'url' => ['site/contacts']],
-                        ],
-                    ]);
-                    ?>
-                </div>
-        </tr>
-        <tr>
-            <div class="wrap">
-                <?php
-                NavBar::begin([
-                    'options' => [
-                        'class' => 'navbar-inverse navbar-toggle',
-                    ],
-                ]);
-                echo Nav::widget([
-                    'options' => ['class' => 'navbar-nav navbar-center'],
-                    'items' => [
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-center'],
+        'items' => [
 
-                        ['label' => 'Персональный транспорт', 'url' => ['category/personal-vehicles']],
-                        ['label' => 'Фитнес и здоровье', 'url' => ['category/fitness-and-health']],
-                        ['label' => 'Фото и видео', 'url' => ['category/photo-and-video']],
-                        ['label' => 'Умный дом', 'url' => ['category/smart-home']],
-                        ['label' => 'Развлечения', 'url' => ['promopages/entertainment']],
-                        ['label' => 'Селфи-стики', 'url' => ['shop/goods/monopods-and-selfie-sticks']],
-                        ['label' => 'Батареи', 'url' => ['shop/goods/portativnye-batarei']],
-                        ['label' => 'Подарки', 'url' => ['promo/presents']],
-                        ['label' => 'Sale', 'url' => ['sale/']],
-                        /*['label' => 'Компания', 'url' => ['/site/about']],
-                        ['label' => 'Contact', 'url' => ['/site/contact']],
-                        Yii::$app->user->isGuest ? (
-                            ['label' => 'Login', 'url' => ['/site/login']]
-                        ) : (
-                            '<li>'
-                            . Html::beginForm(['/site/logout'], 'post')
-                            . Html::submitButton(
-                                'Logout (' . Yii::$app->user->identity->username . ')',
-                                ['class' => 'btn btn-link logout']
-                            )
-                            . Html::endForm()
-                            . '</li>'
-                        )*/
-                    ],
-                ]);
-                NavBar::end();
-                ?>
-        </tr>
-    </table>
-
-
-
+            ['label' => 'Персональный транспорт', 'url' => ['category/personal-vehicles']],
+            ['label' => 'Фитнес и здоровье', 'url' => ['category/fitness-and-health']],
+            ['label' => 'Фото и видео', 'url' => ['category/photo-and-video']],
+            ['label' => 'Умный дом', 'url' => ['category/smart-home']],
+            ['label' => 'Развлечения', 'url' => ['promopages/entertainment']],
+            ['label' => 'Селфи-стики', 'url' => ['shop/goods/monopods-and-selfie-sticks']],
+            ['label' => 'Батареи', 'url' => ['shop/goods/portativnye-batarei']],
+            ['label' => 'Подарки', 'url' => ['promo/presents']],
+            ['label' => 'Sale', 'url' => ['sale/']],
+            /*['label' => 'Компания', 'url' => ['/site/about']],
+            ['label' => 'Contact', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest ? (
+                ['label' => 'Login', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>'
+            )*/
+        ],
+    ]);
+    NavBar::end();
+    ?>
 
     <div class="container">
         <?= Breadcrumbs::widget([
@@ -106,7 +78,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Citrus <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Магаз <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
